9c0b02b53efb9165dd9dbe2a32b1e99f6828b554
JordyCA <jordycastroavendano@gmail.com>
2020-01-05 22:07:12 -0600

    Se ingresan actualizaciones para la base de datos

---
+++ Base_Datos/Catalogo/catalogos_NivelAcademico.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `lasalle`.`cat_nivelacademico` (`idNivelAcademico`, `nivelacademico_tipo`,`nivelacademico_fecha`) VALUES ('1', 'MAESTRÍA',now());
+INSERT INTO `lasalle`.`cat_nivelacademico` (`idNivelAcademico`, `nivelacademico_tipo`,`nivelacademico_fecha`) VALUES ('2', 'LICENCIATURA',now());
+INSERT INTO `lasalle`.`cat_nivelacademico` (`idNivelAcademico`, `nivelacademico_tipo`,`nivelacademico_fecha`) VALUES ('3', 'DOCTORADO',now());
+
