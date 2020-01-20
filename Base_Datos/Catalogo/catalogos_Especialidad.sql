71b517e3f7bbe79e490b80bbc0ecb058b25b11a8
JordyCA <56770908+JordyCA@users.noreply.github.com>
2020-01-20 15:29:44 -0600

    Add files via upload

---
+++ Base_Datos/Catalogo/catalogos_Especialidad.sql
@@ -0,0 +1,10 @@
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('1', 'Ingenieria de software', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('2', 'Psicología', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('3', 'Educación', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('4', 'Mecatronica', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('5', 'Derecho', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('6', 'Maestría en peritajes', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('7', 'Maestría en educación especial', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('8', 'Maestría en educación especial', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('9', 'Doctorado en investigación', now());
+INSERT INTO `lasalle`.`cat_especialidad` (`idcat_especialidad`, `especialidad_nombre`, `especialidad_fecha`) VALUES ('10', 'Doctorado en tecnologías', now());
